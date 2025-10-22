b26588266b2642652541b7b650e96ba4e625905d
Vyktor <victureca3@gmail.com>
2025-10-22 23:31:11 +0200

    Particulas

--- Code/Assets/Scripts/ShootGallery/TargetCollider.cs
+++ Code/Assets/Scripts/ShootGallery/TargetCollider.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class TargetCollider : MonoBehaviour
 {
     public float m_score;
+    public ParticleSystem m_smokeParticle;
     
     public float Hit()
     {
+        ParticleSystem smoke = Instantiate(m_smokeParticle,transform.position,Quaternion.identity);
+
+        smoke.Play();
+        m_smokeParticle.Play();
         gameObject.SetActive(false);
         return m_score;
     }
